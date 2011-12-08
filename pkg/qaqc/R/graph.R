bbd501b2cf452a170d6e25e6fbcf40dfaaec2786
bwhitcher <bwhitcher@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-08 12:05:09 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rigorous@241 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qaqc/R/graph.R
+++ pkg/qaqc/R/graph.R
@@ -54,3 +54,32 @@ graphSingleSlice <- function(nim, uid, modality, imgPath, htmlFile,
   invisible()
 }
 
+graphImagePatientPosition <- function(nim, uid, imagePositionPatient,
+                                      sliceLocation, imgPath, htmlFile,
+                                      width=512, height=512, ...) {
+  graph <- paste(uid, "ipp.png", sep="_") 
+  png(file.path(imgPath, graph)) # bitmap(file.path(imgPath, graph), taa=4, gaa=4)
+  par(mfrow=c(2,2), oma=c(0,0,2,0), mex=0.85)
+  plot(str2time(contentTime)$time, ipp[,1], xlab="Time (julian)",
+       ylab="ImagePositionPatient[, 1]", cex=0.5)
+  plot(str2time(contentTime)$time, ipp[,2], xlab="Time (julian)",
+       ylab="ImagePositionPatient[, 2]", cex=.5)
+  plot(str2time(contentTime)$time, ipp[,3], xlab="Time (julian)",
+       ylab="ImagePositionPatient[, 3]", cex=.5)
+  mtext(outer=TRUE, side=3, fname, cex=1.2)
+  sliceLocation <- extractHeader(uid.dcm$hdr, "SliceLocation")
+  plot(str2time(contentTime)$time, sliceLocation, xlab="Time (julian)",
+           ylab="SliceLocation", cex=.5)
+
+  par(mfrow=c(1,1), mar=c(5,4,4,2)+.5, bg="white")
+  dSL <- abs(diff(imagePositionPatient[,movingDimensions]))
+  plot(dSL, ylim=range(range(dSL) * 1.5, 0, 10), 
+       xlab="Index", ylab="mm", main="Difference in Slice Location")
+  dev.off()
+  graphImage <- hwriteImage(graph, width=width, height=height, center=TRUE)
+  graphCaption <- "Use this graph to detect inconsistent slice thickness.  All points should be equal and constant."
+  hwrite(c(graphImage, graphCaption), htmlFile, br=TRUE, dim=c(2,1),
+         center=TRUE, border=0, style="text-align:center")
+  invisible()
+}
+
