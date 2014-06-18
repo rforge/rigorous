ff56d5df557d619f4abeef3594f9e2ba8197a42b
ktabelow <ktabelow@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-06-18 08:26:43 +0000

    bugfix for readAFNI()
    R CMD check runs (example file is readable now)
    
    k
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rigorous@334 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/oro.nifti/R/afniS4.R
+++ pkg/oro.nifti/R/afniS4.R
@@ -420,8 +420,8 @@ readAFNI <- function(fname, vol=NULL, verbose=FALSE, warn=-1, call=NULL) {
   ## Constructor S4 object
   dim(dataCube) <- c(ddim, length(vol))
   values$DATASET_RANK[2] <- length(vol)
-  values$TAXIS_NUMS[1] <- length(vol)
-  values$BRICK_TYPES <- values$BRICK_TYPES[vol]
+  if (!is.null(values$TAXIS_NUMS)) values$TAXIS_NUMS[1] <- length(vol)
+  if (!is.null(values$BRICK_TYPES)) values$BRICK_TYPES <- values$BRICK_TYPES[vol]
   nim <- new("afni",
              dataCube,
              DATASET_RANK = values$DATASET_RANK,
